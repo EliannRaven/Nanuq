4dd59e8509e354d693464c8c156ab6e6ee7c76b4
EliannRaven <nicolas.habisreutinger@gmail.com>
2024-01-04 11:15:59 +0100

    Set-up static and media file

---
+++ Nanuq/Suivi/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import intraHome
+
+urlpatterns = [
+    path('', intraHome, name='intraHome'),
+]
+
+#C:\Users\nicoh\Documents\Computer_science\Portfolio\Nanuq\Nanuq\Nanuq\static
+#C:\Users\nicoh\Documents\Computer_science\Portfolio\Nanuq\Nanuq\static
--- Nanuq/Suivi/views.py
+++ Nanuq/Suivi/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def intraHome(request):
+    return render(request, 'Suivi/intraHome.html')
